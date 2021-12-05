3ac39bb32799766fffe19e3ee47ab985761e67e7
rendrap <rendrapradana@gmail.com>
2021-12-05 19:43:04 +0700

    init

---
+++ src/Application.tsx
@@ -0,0 +1,26 @@
+type NameTagProps = {
+
+  
+  name: string;
+};
+
+const NameTag = function ({ name }: NameTagProps) {
+  return (
+    <main>
+      <header>
+        <h1>Hello</h1>
+        <p>My Name Is</p>
+      </header>
+      <section className="display-name">
+        <p>{name}</p>
+      </section>
+      <footer />
+    </main>
+  );
+};
+
+const Application = function () {
+  return <NameTag name="Steve" />;
+};
+
+export default Application;
