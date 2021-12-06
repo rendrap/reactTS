9fadc4ae1cc3663ab125ac9d0b0d27e320528bca
rendrap <rendrapradana@gmail.com>
2021-12-06 14:45:38 +0700

    function types

--- src/Application.tsx
+++ src/Application.tsx
@@ -1,6 +1,48 @@
+import React from 'react';
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
 type NameTagProps = {
   name: string;
 };
+
+type GroceryListProps = {
+  items: string[];
+  status: 'loading' | 'error' | 'success';
+};
+
+type Item = {
+  item: {
+    id: string;
+    title: string;
+  };
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  anObject: object;
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  anotherObject: {};
+};
+
+type Dictionary = {
+  [key: number]: string;
+};
+
+type ItemHash = {
+  [key: string]: Item;
+};
+
+type ContrivedExampleProps = {
+  // doesn't take any args, doesn't return anything
+  onHover: () => void;
+  // Takes a number, return nothing
+  onChange: () => void;
+  // Take an event based on clicking on a buuton
+  onClick(event: React.MouseEvent<HTMLButtonElement>): void;
+};
+
+const add = (a: number, b: number): number => a + b;
+
+function substract(a: number, b: number): number {
+  return a - b;
+}
 
 const NameTag = (props: NameTagProps) => (
   <main>
